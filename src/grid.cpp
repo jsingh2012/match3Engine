b1da90fe8ee96ee66bb7d1e0bb9ac5f789e3a03b
EC2 Default User <ec2-user@ip-172-31-32-161.us-west-2.compute.internal>
2017-09-24 09:12:54 +0000

    grid based on node.

--- src/grid.cpp
+++ src/grid.cpp
@@ -3,11 +3,42 @@
 #include<string.h>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
+using namespace std;
+
+class node
+{
+	static int id;
+	public:
+	int posX;
+	int posY;
+	int type;
+	node(){
+		id++;
+		type = 0;
+	}
+	node(int x,int y, int _type)
+	{
+		id++;
+		posX = x;
+		posY = y;
+		type = _type;
+	}
+
+	bool operator==(node& rhs)const
+	{
+		if (this->type ==rhs.type)
+			return true;
+		return false;
+	}
+};
+
+int node::id = 0;
 
 class Grid
 {
-	int grid[100][100];
+	node grid[100][100];
 	static Grid *instance;
 	int rowCount;
 	int colCount;
@@ -16,7 +47,6 @@ class Grid
 	Grid(const int _rowCount ,const int _colCount )
 	{
 		// dynamically allocate memory of size M*N
-		memset(grid, 0, sizeof(grid[0][0]) * _rowCount * _colCount);
 		this->rowCount = _rowCount;
 		this->colCount = _colCount;
 	}
@@ -27,7 +57,7 @@ class Grid
 		{
 			for(int j = 0; j < colCount; j++)
 			{
-				printf("%2d ", grid[i][j]);
+				printf("(%2d,%2d)%2d ",i,j, grid[i][j].type);
 			}
 			printf("\n");
 		}
@@ -41,9 +71,96 @@ class Grid
 		{
 			for(int j = 0; j < colCount; j++)
 			{
-				grid[i][j] = std::rand() % maxDiffTileType;
+				grid[i][j].type = std::rand() % maxDiffTileType;
+				grid[i][j].posX = i;
+				grid[i][j].posY = j;
 			}
 		}
+	}
+
+	void checkAllMatches()
+	{
+		for(int i = 0 ; i < rowCount; i++)
+		{
+			for(int j = 0; j < colCount; j++)
+			{
+				bool matched = false;
+				vector <node*> matchesNodes;
+				printf("START getMatchAtPos at := %2d %2d := \n", i, j);
+				matchesNodes = getMatchAtPos(i, j);
+				for (auto&& node : matchesNodes)
+				{
+					printf("%d ", node->type);
+					matched = true;
+				}
+				printf("\n");
+				if(matched)
+					printGrid();
+				matched = false;	
+			printf("END\n");
+			}
+		}
+		
+	}	
+
+	vector <node*> getMatchAtPos(int posX, int posY)
+	{
+		int maxLeft = 0;
+		int maxRight = 0;
+		int maxUp = 0;
+		int maxDown = 0;
+		int matchingTiles = 0;
+		vector <node*> matchedNode;
+		for(int i = posX - 1; i >= 0; i--)	
+		{
+			if(grid[posX][posY] == grid[i][posY])
+				maxUp++;
+			else
+				break;
+		}
+		
+		for(int i = posX + 1; i < colCount; i++)	
+		{
+			if(grid[posX][posY] == grid[i][posY])
+				maxDown++;
+			else
+				break;
+		}
+		
+		for(int j = posY - 1; j >= 0; j--)	
+		{
+			if(grid[posX][posY] == grid[posX][j])
+				maxLeft++;
+			else
+				break;
+		}
+		
+		for(int j = posY + 1; j < rowCount; j++)	
+		{
+			if(grid[posX][posY] == grid[posX][j])
+				maxRight++;
+			else
+				break;
+		}
+		
+		if((maxLeft + maxRight + 1) >= 3){
+			printf("Match in horizontal Direction at %2d %2d := %d\n", posX, posY, (maxLeft + maxRight + 1));
+			matchingTiles = maxLeft + maxRight + 1;
+			for(int i = posY - maxLeft; i <= posY + maxRight; i++)
+			{
+				matchedNode.push_back(&grid[posX][i]);	
+			}
+		}
+		if((maxUp + maxDown + 1) >= 3) {
+			printf("Match in Vertical Direction at %2d %2d := %d\n", posX, posY, (maxUp + maxDown + 1));
+			matchingTiles += maxUp + maxDown + 1;
+			
+			for(int i = posX - maxUp; i <= posX + maxDown; i++)
+			{
+				matchedNode.push_back(&grid[i][posY]);	
+			}
+		}
+		return matchedNode;
 	}
 };
 
@@ -52,8 +169,9 @@ class Grid
 int main(){
 	Grid myGrid(10, 10);
 	//myGrid.printGrid();
-	myGrid.fillTheGrid(10);
+	myGrid.fillTheGrid(5);
 	myGrid.printGrid();
+	myGrid.checkAllMatches();
 }
 
 
